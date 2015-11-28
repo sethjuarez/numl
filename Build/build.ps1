925192e413c1cbc03ec069d4b77753b332649443
sethjuarez <me@sethjuarez.com>
2015-11-27 17:55:26 -0800

    working on build

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,16 +1,32 @@
 properties {
+	$workingName = if ($workingName) {$workingName} else {"Working"}
+	
 	$baseDir  = resolve-path ..
 	$buildDir = "$baseDir\Build"
 	$sourceDir = "$baseDir\Src"
 	$toolsDir = "$baseDir\Tools"
 	$docDir = "$baseDir\Doc"
-	$releaseDir = "$baseDir\Release"	
+	$releaseDir = "$baseDir\Release"
+	$workingDir = "$baseDir\$workingName"
 }
+
+framework '4.6x86'
 
 task default -depends Test
 
 task Clean {
-	Write-Host "Clean step!"
+	Write-Host "Setting location to $baseDir"
+	Set-Location $baseDir
+	
+	if (Test-Path -path $workingDir)
+	{
+		Write-Host "Deleting existing working directory $workingDir"
+	
+		Invoke-Command { del $workingDir -Recurse -Force }
+	}
+	
+	Write-Host "Creating working directory $workingDir"
+	New-Item -Path $workingDir -ItemType Directory
 }
 
 task Build -depends Clean {
