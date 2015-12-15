309436022aadb46351588fdd6c9aa2826f518991
sethjuarez <me@sethjuarez.com>
2015-12-14 22:25:07 -0800

    added xproj - still not building

--- Build/build.ps1
+++ Build/build.ps1
@@ -14,11 +14,12 @@ properties {
 	$workingDir = "$baseDir\$workingName"
 	$workingSourceDir = "$workingDir\Src"
 	$builds = @(
-		@{Name = "numl"; TestsName = "numl.Tests"; BuildFunction = "MSBuildBuild"; TestsFunction = "NUnitTests"; Constants=""; FinalDir="Portable"; NuGetDir = "portable-net45+wp80+win8+wpa81+dnxcore50"; Framework="net-4.0"}
+		@{Name = "numl"; TestsName = "numl.Tests"; BuildFunction = "MSBuildBuild"; TestsFunction = "NUnitTests"; FinalDir="Portable"; NuGetDir = "portable-net45+wp80+win8+wpa81+dnxcore50"; Framework="net-4.0"}
 	)
 }
 
 framework '4.6x86'
+include .\psake_helpers.ps1
 
 task default -depends Nuget
 
@@ -29,7 +30,6 @@ task Clean {
 	if (Test-Path -path $workingDir)
 	{
 		Write-Host "Deleting existing working directory $workingDir"
-	
 		Execute-Command -command { del $workingDir -Recurse -Force }
 	}
 	
@@ -45,27 +45,25 @@ task Build -depends Clean {
 	Write-Host
 	Update-AssemblyInfoFiles $workingSourceDir ($majorVersion + '.0.0') $version
 	
-	Update-Project $workingSourceDir\numl\project.json
-	
+
 	foreach ($build in $builds)
 	{
 		$name = $build.Name
 		if ($name -ne $null)
 		{
 			Write-Host -ForegroundColor Green "Building " $name
-			& $build.BuildFunction $build
+			& $build.BuildFunction $build $true
 		}
 	}
 }
 
 task Test -depends Build {
-	Update-Project $workingSourceDir\numl\project.json $false
-
+	
 	foreach ($build in $builds)
 	{
 		if ($build.TestsFunction -ne $null)
 		{
-			& $build.TestsFunction $build
+			& $build.TestsFunction $build $false
 		}
 	}
 }
@@ -126,150 +124,4 @@ task Nuget -depends Build {
 	Write-Host
 	Write-Host "Moving package to $workingDir\NuGet" -ForegroundColor Green
 	move -Path .\*.nupkg -Destination $workingDir\NuGet
-}
-
-function NUnitTests($build)
-{
-	$name = $build.TestsName
-	$finalDir = $build.FinalDir
-	$framework = $build.Framework
-	
-	Write-Host -ForegroundColor Green "Copying test assembly $name to deployed directory"
-	Write-Host
-	robocopy "$workingSourceDir\numl.Tests\bin\Release\$finalDir" $workingDir\Deployed\Bin\$finalDir /MIR /NFL /NDL /NJS /NC /NS /NP /XO | Out-Default
-	
-	Copy-Item -Path "$workingSourceDir\numl.Tests\bin\Release\$finalDir\numl.Tests.dll" -Destination $workingDir\Deployed\Bin\$finalDir\
-	
-	Write-Host -ForegroundColor Green "Running NUnit tests " $name
-	Write-Host
-	exec { .\Tools\NUnit\nunit-console.exe "$workingDir\Deployed\Bin\$finalDir\numl.Tests.dll" /result=$workingDir\$name.xml /trace=Info /labels | Out-Default } "Error running $name tests"
-}
-
-function Update-AssemblyInfoFiles ([string] $workingSourceDir, [string] $assemblyVersionNumber, [string] $fileVersionNumber)
-{
-	$assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-	$fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-	$assemblyVersion = 'AssemblyVersion("' + $assemblyVersionNumber + '")';
-	$fileVersion = 'AssemblyFileVersion("' + $fileVersionNumber + '")';
-	
-	Get-ChildItem -Path $workingSourceDir -r -filter AssemblyInfo.cs | ForEach-Object {
-		Write-Host $workingSourceDir
-		$filename = $_.Directory.ToString() + '\' + $_.Name
-		Write-Host $filename
-		$filename + ' -> ' + $version
-		
-		(Get-Content $filename) | ForEach-Object {
-			% {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
-			% {$_ -replace $fileVersionPattern, $fileVersion }
-			
-		} | Set-Content $filename
-	}
-}
-
-function Update-Project($projectPath) 
-{
-	$json = (Get-Content $projectPath) -join "`n" | ConvertFrom-Json
-	$options = @{"warningsAsErrors" = $true; "define" = ((GetConstants "dotnet") -split ";") }
-	Add-Member -InputObject $json -MemberType NoteProperty -Name "compilationOptions" -Value $options -Force
-	ConvertTo-Json $json -Depth 10 | Set-Content $projectPath
-}
-
-function GetConstants($constants)
-{
-	return "CODE_ANALYSIS;TRACE;$constants"
-}
-
-function MSBuildBuild($build)
-{
-	$name = $build.Name
-	$finalDir = $build.FinalDir
-	
-	Write-Host
-	Write-Host "Restoring $workingSourceDir\$name.sln" -ForegroundColor Green
-	[Environment]::SetEnvironmentVariable("EnableNuGetPackageRestore", "true", "Process")
-	exec { .\Tools\NuGet\NuGet.exe update -self }
-	exec { .\Tools\NuGet\NuGet.exe restore "$workingSourceDir\$name.sln" -verbosity detailed -configfile $workingSourceDir\nuget.config | Out-Default } "Error restoring $name"
-	
-	$constants = GetConstants $build.Constants
-	
-	Write-Host
-	Write-Host "Building $workingSourceDir\$name.sln" -ForegroundColor Green
-	if ($name -match 'Tests') { 
-		Write-Host "NO DOCS $name" -ForegroundColor Red
-		#exec { msbuild "/t:Clean;Rebuild" /p:Configuration=Release /p:OutputPath=bin\Release\$finalDir\ "/p:TreatWarningsAsErrors=$treatWarningsAsErrors" "/p:VisualStudioVersion=14.0" /p:DefineConstants=`"$constants`" "$workingSourceDir\$name.sln" | Out-Default } "Error building $name"
-	} else {
-		Write-Host "YES DOCS $name" -ForegroundColor Red
-		#exec { msbuild "/t:Clean;Rebuild" /p:Configuration=Release /p:OutputPath=bin\Release\$finalDir\ "/p:TreatWarningsAsErrors=$treatWarningsAsErrors" "/p:VisualStudioVersion=14.0" "/p:documentationFile=bin\Release\$finalDir\$name.xml" /p:DefineConstants=`"$constants`" "$workingSourceDir\$name.sln" | Out-Default } "Error building $name"	
-	}
-}
-
-function GetVersion($majorVersion)
-{
-	$now = [DateTime]::Now
-	
-	$year = $now.Year - 2000
-	$month = $now.Month
-	$totalMonthsSince2000 = ($year * 12) + $month
-	$day = $now.Day
-	$minor = "{0}{1:00}" -f $totalMonthsSince2000, $day
-	
-	$hour = $now.Hour
-	$minute = $now.Minute
-	$revision = "{0:00}{1:00}" -f $hour, $minute
-	
-	return $majorVersion + "." + $minor
-}
-
-function Edit-XmlNodes {
-    param (
-        [xml] $doc,
-        [string] $xpath = $(throw "xpath is a required parameter"),
-        [string] $value = $(throw "value is a required parameter")
-    )
-    
-    $nodes = $doc.SelectNodes($xpath)
-    $count = $nodes.Count
-
-    Write-Host "Found $count nodes with path '$xpath'"
-    
-    foreach ($node in $nodes) {
-        if ($node -ne $null) {
-            if ($node.NodeType -eq "Element")
-            {
-                $node.InnerXml = $value
-            }
-            else
-            {
-                $node.Value = $value
-            }
-        }
-    }
-}
-
-function Execute-Command($command) 
-{
-	$currentRetry = 0
-	$success = $false
-	do 
-	{
-		try
-		{
-			& $command
-			$success = $true
-		}
-		catch [System.Exception]
-		{
-			if ($currentRetry -gt 5) 
-			{
-				throw $_.Exception.ToString()
-			} 
-			else 
-			{
-				write-host "Retry $currentRetry"
-				Start-Sleep -s 1
-			}
-			$currentRetry = $currentRetry + 1
-		}
-	} 
-	while (!$success)
 }
