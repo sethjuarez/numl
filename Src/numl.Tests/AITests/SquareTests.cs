086e7b9c6e5132c01ab433d4148d3d49383ba5c2
sethjuarez <me@sethjuarez.com>
2015-12-23 22:04:14 -0800

    added testing infrastructure for new AI functionality

---
+++ Src/numl.Tests/AITests/SquareTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using numl.AI;
+
+namespace numl.Tests.AITests
+{
+    [TestFixture, Category("AI")]
+    public class SquareTests
+    {
+        private static int[] CreateSquare(int i)
+        {
+            var square = new int[9];
+            square[i] = 0;
+            for (int j = 0; j < 8; j++)
+            {
+                if (j >= i)
+                    square[j + 1] = j + 1;
+                else
+                    square[j] = j + 1;
+            }
+            return square;
+        }
+
+        private static void PrintSolution(IEnumerable<ISuccessor> solution)
+        {
+            foreach (var successor in solution)
+            {
+                //Console.WriteLine(successor.Action);
+                //Console.WriteLine(successor.State);
+            }
+        }
+
+        [Test]
+        public void Test_Square_Expansion()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                int[] square = CreateSquare(i);
+                IState init = new Square(square);
+                Console.WriteLine(init.ToString());
+                foreach (var s in init.GetSuccessors())
+                {
+                    Console.WriteLine("---------\n{0} ({1}{2})", s.Action, s.Cost, s.State.IsTerminal ? ", Goal" : "");
+                    Console.WriteLine(s.State);
+                }
+
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+
+        [Test]
+        public void Test_Easy_Square_BFS()
+        {
+            IState init = new Square(new[] { 1, 4, 2, 3, 5, 8, 6, 0, 7 });
+            Console.WriteLine(init);
+            SimpleSearch bfs = new SimpleSearch(new BreadthFirstSearch());
+            var solution = bfs.Find(init);
+
+            if (solution) PrintSolution(bfs.Solution);
+            Assert.IsTrue(solution);
+        }
+
+
+        [Test]
+        public void Test_Hard_AStar()
+        {
+            IState init = new Square(new[] { 1, 2, 3, 4, 5, 6, 7, 0, 8 });
+
+            AStarSearch strategy = new AStarSearch()
+            {
+                Heuristic = s => s.Heuristic()
+            };
+
+            SimpleSearch a = new SimpleSearch(strategy);
+            var solution = a.Find(init);
+            if (solution) PrintSolution(a.Solution);
+            Assert.IsTrue(solution);
+        }
+
+    }
+}
---
+++ Src/numl.Tests/AITests/TicTacToe.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using numl.AI;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace numl.Tests.AITests
+{
+    public class TicTacToe : IAdversarialState
+    {
+        public string Id { get; private set; }
+        public bool IsTerminal { get; private set; }
+        public double Utility { get; private set; }
+        public bool Player { get; private set; }
+
+        private readonly int[] _board = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public TicTacToe(bool player)
+        {
+            Id = Guid.NewGuid().ToString();
+            IsTerminal = false;
+            Player = player;
+        }
+
+        public TicTacToe(bool player, int[] board)
+            : this(player)
+        {
+            Id = Guid.NewGuid().ToString();
+            _board = board;
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            Utility = 0;
+            IsTerminal = false;
+
+            var wins = new[]
+            {
+                new[] {0, 1, 2},
+                new[] {3, 4, 5},
+                new[] {6, 7, 8},
+                new[] {0, 3, 6},
+                new[] {1, 4, 7},
+                new[] {2, 5, 8},
+                new[] {0, 4, 8},
+                new[] {2, 4, 6},
+            };
+
+            foreach (var w in wins)
+            {
+                var u = Calculate(w);
+                if (u != 0)
+                {
+                    Utility = u;
+                    IsTerminal = u != 0;
+                    return;
+                }
+            }
+        }
+
+        private int Calculate(int[] w)
+        {
+            if (w.Length != 3) throw new InvalidOperationException("Needs to be three!");
+
+            if (System.Math.Abs(_board[w[0]] + _board[w[1]] + _board[w[2]]) == 3)
+                return _board[w[0]] < 0 ? -1 : 1;
+            else
+                return 0;
+        }
+        
+        public IEnumerable<ISuccessor> GetSuccessors()
+        {
+            for(int i = 0; i < _board.Length; i++)
+            {
+                if(_board[i] == 0)
+                {
+                    var play = Player ? 1 : -1;
+                    var newBoard = (int[])_board.Clone();
+                    newBoard[i] = play;
+                    yield return new TicTacToeMove(new TicTacToe(!Player, newBoard), i.ToString());
+                }
+            }
+        }
+
+        public bool IsEqualTo(IState state)
+        {
+            if (state == null) return false;
+            if (!(state is TicTacToe)) return false;
+
+            TicTacToe tictactoe = (TicTacToe)state;
+            for (int i = 0; i < _board.Length; i++)
+                if (_board[i] != tictactoe._board[i])
+                    return false;
+
+            return true;
+        }
+
+        public IAdversarialState Reset()
+        {
+            return new TicTacToe(Player);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < _board.Length; i++)
+            {
+                var p = '_';
+                if (_board[i] < 0) p = 'o';
+                if (_board[i] > 0) p = 'x';
+
+                sb.Append(string.Format(" {0} ", p));
+                if ((i + 1) % 3 == 0) sb.Append(" \n");
+            }
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return IsEqualTo(obj as IState);
+        }
+
+        public override int GetHashCode()
+        {
+            return _board.GetHashCode();
+        }
+
+        public double Heuristic()
+        {
+            // not applicable in this case
+            return 1;
+        }
+    }
+
+
+    public class TicTacToeMove : ISuccessor
+    {
+
+        public TicTacToeMove(IState state, string action)
+        {
+            State = state;
+            Action = action;
+        }
+
+        public double Cost
+        {
+            get { return 1; }
+        }
+
+        public string Action { get; private set; }
+
+        public IState State { get; private set; }
+
+    }
+
+}
