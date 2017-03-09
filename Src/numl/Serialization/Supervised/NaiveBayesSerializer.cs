2deb0073e3eed426cab5aea81ae5244f9097554d
Seth Juarez <me@sethjuarez.com>
2017-03-08 16:37:06 -0800

    working on last Naive Bayes serializer

---
+++ Src/numl/Serialization/Supervised/NaiveBayesSerializer.cs
@@ -0,0 +1,46 @@
+﻿using numl.Supervised.NaiveBayes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace numl.Serialization.Supervised.NaiveBayes
+{
+    public class NaiveBayesSerializer : ModelSerializer
+    {
+        public override bool CanConvert(Type type)
+        {
+            return typeof(NaiveBayesModel).IsAssignableFrom(type);
+        }
+
+        public override object Create()
+        {
+            return new NaiveBayesModel();
+        }
+
+        public override object Read(JsonReader reader)
+        {
+            if (reader.IsNull()) return null;
+            else
+            {
+                var d = base.Read(reader) as NaiveBayesModel;
+
+                // naive reads properties here
+
+                return d;
+            }
+        }
+
+        public override void Write(JsonWriter writer, object value)
+        {
+            if (value == null) writer.WriteNull();
+            else
+            {
+                var d = value as NaiveBayesModel;
+                base.Write(writer, d);
+
+                // naive bates writes here
+            }
+        }
+    }
+}
